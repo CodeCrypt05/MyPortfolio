811c785f6e2d678cb1ecf0d036911f161f9a2213
vaibhav <vaibhavwaghre@gmail.com>
2023-10-27 17:02:09 +0530

    work container design complete

---
+++ lib/presentation/pages/containers/about/about.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:my_portfolio/presentation/pages/containers/about/about_desktop.dart';
+import 'package:my_portfolio/presentation/pages/containers/about/about_mobile.dart';
+import 'package:my_portfolio/presentation/pages/containers/about/about_tablet.dart';
+import 'package:responsive_builder/responsive_builder.dart';
+
+class About extends StatelessWidget {
+  const About({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenTypeLayout.builder(
+      mobile: (BuildContext context) => AboutMobile(),
+      tablet: (BuildContext context) => AboutTablet(),
+      desktop: (BuildContext context) => AboutDesktop(),
+    );
+  }
+}
---
+++ lib/presentation/pages/containers/about/about_desktop.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AboutDesktop extends StatelessWidget {
+  const AboutDesktop({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(left: 160, right: 160, top: 40),
+    );
+  }
+}
